f83076a8708131046bb247e869e621e809bc42f3
Kroy665 <90123639+Kroy665@users.noreply.github.com>
2021-11-07 12:25:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const AuthRoute = require('./Routes/Auth.route')
 
 const app = express()
 app.use(cors(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://auth-route-test.herokuapp.com"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
