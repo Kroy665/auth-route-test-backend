1e1ee1f49a3bb7912c5f640e1f4f83a44ddb9013
Kroy665 <90123639+Kroy665@users.noreply.github.com>
2021-11-09 19:33:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,7 @@ var cors = require('cors')
 const AuthRoute = require('./Routes/Auth.route')
 
 const app = express()
-app.use(cors(), function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "https://auth-route-test.herokuapp.com/auth"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
